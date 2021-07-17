7ec7e459cf73d7a1933e432374ab2f31226d7eef
Meghan Cleland <mplscleland@gmail.com>
2021-07-16 20:14:24 -0500

    installed redux react-redux and redux-logger. Setup imports on index.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
+      
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import { applyMiddleware, combineReducers } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//reducers go here
+
+//store goes here
+const storeInstance = createStore(
+    combineReducers({
+
+    }),
+    applyMiddleware(logger)
+);
+//wrap App in Provider to make store available 
+ReactDOM.render(
+<React.StrictMode>
+<Provider store={storeInstance}>
+<App />
+</Provider>
+</React.StrictMode>    
+, document.getElementById('root'));
 registerServiceWorker();
