150f016778b7731820db1c29772e6d1b6993f07c
Meghan Cleland <mplscleland@gmail.com>
2021-07-16 21:26:46 -0500

    added server/router info and began PageOne imports

---
+++ src/components/PageOne/PageOne.jsx
@@ -0,0 +1,4 @@
+import "./PageOne.css";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
